37b23550e256218d9f036613443c72b1113ed5eb
Jakub Karas <jakub.karas@mycroftmind.com>
2022-11-22 21:26:14 +0100

    manual movement added

---
+++ manual_movement.py
@@ -0,0 +1,28 @@
+from game import Game
+import pygame as pg
+import path
+
+def manual_movement(map):
+    game = Game(path.MAPS / map)
+    clock = pg.time.Clock()
+    FPS = 60
+
+    run = True
+    while run:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+                break
+        
+        clock.tick(FPS)
+
+        state = game.get_state()
+        action = game.move_player()
+
+        game.draw(game.MAP.gates[game.next_gate], True, True)
+                
+        # print((state, action))
+        if game.wall_collision():
+            game.reset()
+        game.gate_collision()
+    pg.quit()
