a3aeeec17472aeedbb0bb55782dc5cd53855565a
Jakub Karas <jakub.karas@mycroftmind.com>
2022-11-22 21:27:46 +0100

    main file uses defined movement and training functions

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-import pygame as pg
-from game import Game
-import path
-from q_learning import QLearning
+from testing import testing
+from manual_movement import manual_movement
+from manual_training import manual_training
+from auto_training import auto_training
 
-game = Game(path.MAPS / 'simpler')
-agent = QLearning(game, 100000, 64, 0.1)
 
-clock = pg.time.Clock()
-FPS = 60
-
-run = True
-i = 0
-while run:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            run = False
-            break
-    
-    clock.tick(FPS)
-
-    # state = game.get_state()
-    # action = agent.get_action(state)
-
-    game.draw(game.MAP.gates[game.next_gate], True)
-    
-    if i < agent.memory.count():
-        game.make_action(agent.memory.buffer[i][1])
-    else:
-        game.move_player()
-
-    print(game.get_state())
-    if game.wall_collision():
-        game.reset()
-    game.gate_collision()
-    i += 1
-
-pg.quit()
+if __name__ == "__main__":
+    '''Run only one function'''
+    # manual_movement('simpler')
+    # auto_training(1000000)
+    # testing()
+    # manual_training()
+    pass
