d1948fab35ae940e42afd33cbc8aeeed313656ed
Jakub Karas <jakub.karas@mycroftmind.com>
2022-11-24 12:08:20 +0100

    structure modified

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from testing import testing
 from manual_movement import manual_movement
 from manual_training import manual_training
 from auto_training import auto_training
+from car import CarActions
 
 
 if __name__ == "__main__":
     '''Run only one function'''
     # manual_movement('simpler')
     # auto_training(1000000)
-    # testing()
+    # testing('episode_xx')
     # manual_training()
     pass
