053f4654fcd201a1914665cc7c56b772adcdeb3c
Jakub Karas <jakub.karas@mycroftmind.com>
2022-11-28 12:33:14 +0100

    the memory is rebuild using numpy array as oppose to collections.deque, I was experiencing RAM overflow because of the stupid way deque adds data

--- memory.py
+++ memory.py
@@ -1,48 +1,78 @@
-from collections import deque
 import numpy as np
 import path
 import pandas as pd
 
 class Memory:
-    def __init__(self, max_size):
+    def __init__(self, max_size, state_size):
         self.max_size = max_size
-        self.buffer = deque(maxlen=self.max_size)
+        self.state_size = state_size
+        self.counter = 0 # a counter of written experiences
+        # setup buffers
+        self.states = np.zeros((self.max_size, self.state_size))
+        self.actions = np.zeros(self.max_size)
+        self.rewards = np.zeros(self.max_size)
+        self.next_states = np.zeros((self.max_size, self.state_size))
+        self.terminated = np.zeros(self.max_size, dtype=bool)
 
     def add(self, state, action, reward, next_state, is_episode_finished):
-        self.buffer.append((state, action, reward, next_state, is_episode_finished))
+        '''Add an experience to memory'''
+        # find index to overwrite
+        index = self.counter % self.max_size
+        # overwrite data
+        self.states[index, :] = state
+        self.actions[index] = action
+        self.rewards[index] = reward
+        self.next_states[index, :] = next_state
+        self.terminated[index] = is_episode_finished
+        # increase the counter
+        self.counter += 1
 
     def sample(self, batch_size):
-        buffer_size = len(self.buffer)
+        '''Retrieve batch of experience data of specified size'''
+        # find the random indeces of experiences
+        buffer_size = self.count()
         rnd_indeces = np.random.choice(np.arange(buffer_size), size=batch_size, replace=False)
-        states = np.concatenate([self.buffer[i][0] for i in rnd_indeces])
-        actions = [self.buffer[i][1] for i in rnd_indeces]
-        rewards = [self.buffer[i][2] for i in rnd_indeces]
-        next_states = np.concatenate([self.buffer[i][3] for i in rnd_indeces])
-        terminated = [self.buffer[i][4] for i in rnd_indeces]
-        return states, actions, rewards, next_states, np.array(terminated)
+        # retrieve the data
+        states = self.states[rnd_indeces, :]
+        actions = self.actions[rnd_indeces]
+        rewards = self.rewards[rnd_indeces]
+        next_states = self.next_states[rnd_indeces, :]
+        terminated = self.terminated[rnd_indeces]
+        return states, actions, rewards, next_states, terminated
 
     def count(self):
-        return len(self.buffer)
+        return self.counter if self.counter < self.max_size else self.max_size
 
     def save(self, file = path.MEMORY / 'memory.csv'):
+        '''Save the memory contents to a csv file'''
         count = self.count()
         if count == 0:
             raise Exception("Nothing to save.")
-        state_size = self.buffer[0][0].shape[1]
-        
+        # the process of saving is done using the pandas dataframe
         dataframe = pd.DataFrame()
-        for i in range(state_size):
-            dataframe['state_{}'.format(i)] = [self.buffer[r][0][0, i] for r in range(count)]
-        dataframe['action'] = [self.buffer[r][1] for r in range(count)]
-        dataframe['reward'] = [self.buffer[r][2] for r in range(count)]
-        for i in range(state_size):
-            dataframe['next_state_{}'.format(i)] = [self.buffer[r][3][0, i] for r in range(count)]
-        dataframe['is_episode_finished'] = [self.buffer[r][4] for r in range(count)]
+        # create a column in the dataframe for each variable
+        for i in range(self.state_size):
+            dataframe['state_{}'.format(i)] = self.states[:, i]
+        dataframe['action'] = self.actions
+        dataframe['reward'] = self.rewards
+        for i in range(self.state_size):
+            dataframe['next_state_{}'.format(i)] = self.next_states[:, i]
+        dataframe['is_episode_finished'] = self.terminated
+        # save it
         dataframe.to_csv(file, index=False)
 
     def load(self, file = path.MEMORY / 'memory.csv'):
+        '''Load memory from a csv file'''
+        # the process of loading is done using the pandas dataframe
         dataframe = pd.read_csv(file)
-        state_size = int((dataframe.shape[1] - 3) / 2)
         for i in range(dataframe.shape[0]):
             row = dataframe.iloc[i]
-            self.add(np.array(row[0:state_size]), row[state_size], row[state_size + 1], np.array(row[state_size + 2:-2]), row[-1])
+            # add an experience to memory, faster way would probably be to insert the whole columns,
+            # but since this function is run in O(const.) times, it is not that important
+            self.add(
+                np.array(row[0:self.state_size]).reshape((1, -1)),
+                row[self.state_size],
+                row[self.state_size + 1],
+                np.array(row[self.state_size + 2:-1]).reshape((1, -1)),
+                row[-1]
+            )
