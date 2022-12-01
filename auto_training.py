6b53b2e7a4c2785c1a45943b72d620e504984cb8
Jakub Karas <jakub.karas@mycroftmind.com>
2022-12-01 18:06:09 +0100

    autotraining formalized

--- auto_training.py
+++ auto_training.py
@@ -2,10 +2,14 @@ from game import Game
 from q_learning import QLearning
 import path
 
-def auto_training(training_episodes, load_dir = None):
-    game = Game(path.MAPS / 'simpler')
-    agent = QLearning(game, 10000000, 1000, 10000, 0.9, max_epsilon=1, gamma=0.95)
+def auto_training(training_episodes, load_dir = None, model_only = False, map = 'simpler', max_steps = 500,
+                  policy_steps = 4, memory_size = 5000, training_batch_size = 100, steps_to_align = 1000,
+                  learning_rate = 0.4, gamma = 0.5, min_epsilon = 0.025, max_epsilon = 1, file = 'end'):
+    '''This function serves to make hyperparameter setup easier.'''
+    game = Game(path.MAPS / map)
+    agent = QLearning(game, memory_size, training_batch_size, 1 if load_dir and not model_only else memory_size,
+                      steps_to_align, learning_rate, gamma=gamma, min_epsilon=min_epsilon, max_epsilon=max_epsilon)
     if load_dir:
-        agent.load(load_dir, True)
-    agent.train(training_episodes, 3600, 4)
-    agent.save('end')
+        agent.load(load_dir, model_only)
+    agent.train(training_episodes, max_steps, policy_steps)
+    agent.save(file)
