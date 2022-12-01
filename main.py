bbe1ee5907a3f18a0532c9ff4c131c80b596b9aa
Jakub Karas <jakub.karas@mycroftmind.com>
2022-12-01 19:40:02 +0100

    logger is set up here

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from testing import testing
 from manual_movement import manual_movement
 from manual_training import manual_training
@@ -6,9 +7,10 @@ from car import CarActions
 
 
 if __name__ == "__main__":
-    '''Run only one function'''
+    '''Uncomment a function'''
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(format = '\n%(asctime)s %(module)s %(levelname)s: %(message)s', datefmt = '%I:%M:%S %p', level = logging.DEBUG)
     # manual_movement('simpler')
-    # auto_training(1000000)
-    # testing('episode_xx')
-    # manual_training()
-    pass
+    # auto_training(50000, model_only=False)
+    # testing('episode_1060')
+    # manual_training(CarActions.FORWARD.value)
