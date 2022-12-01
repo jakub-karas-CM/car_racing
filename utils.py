9baacf7eebd058202d1bf898361f831fb4ca1857
Jakub Karas <jakub.karas@mycroftmind.com>
2022-12-01 18:28:41 +0100

    comments added

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import math
-
+'''Basic utilities need for the computations. Mostly self-explanatory.'''
 def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     return pg.transform.scale(img, size)
@@ -13,6 +13,7 @@ def rotate_image(win, image, top_left, angle):
     win.blit(rotated_image, new_rectangle.topleft)
 
 def lines_collided(x1, y1, x2, y2, x3, y3, x4, y4):
+    '''only bool check'''
     denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
     if denominator == 0:
         return False
@@ -23,9 +24,11 @@ def lines_collided(x1, y1, x2, y2, x3, y3, x4, y4):
     return False
 
 def distance_to_line(x, y, line):
+    '''point to line'''
     return abs((line.x2 - line.x1) * (line.y1 - y) - (line.y2 - line.y1) * (line.x1 - x)) / math.sqrt((line.x2 - line.x1) ** 2 + (line.y2 - line.y1) ** 2)
 
 def get_collision_point(x1, y1, x2, y2, x3, y3, x4, y4):
+    '''of two lines'''
     denominator = ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1))
     if denominator == 0:
         return None
@@ -38,4 +41,5 @@ def get_collision_point(x1, y1, x2, y2, x3, y3, x4, y4):
     return None
 
 def distance(x1, y1, x2, y2):
+    '''point to point'''
     return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
