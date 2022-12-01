5b7d0e3629ada34ca2cff4c2014bcc938fd0350d
Jakub Karas <jakub.karas@mycroftmind.com>
2022-12-01 18:59:51 +0100

    comments added, the number of times the experience has been seen already has an impact on the force of it

--- memory.py
+++ memory.py
@@ -3,10 +3,13 @@ import path
 import pandas as pd
 
 class Memory:
+    '''The experience memory of the AI.'''
     def __init__(self, max_size, state_size):
         self.max_size = max_size
         self.state_size = state_size
         self.counter = 0 # a counter of written experiences
+        # initialize the vector of number of times the network has learned from an experience
+        self.seen = np.zeros(self.max_size, dtype=np.int32)
         # setup buffers
         self.states = np.zeros((self.max_size, self.state_size))
         self.actions = np.zeros(self.max_size, dtype=np.int32)
@@ -18,6 +21,8 @@ class Memory:
         '''Add an experience to memory'''
         # find index to overwrite
         index = self.counter % self.max_size
+        # reset seen times
+        self.seen[index] = 0
         # overwrite data
         self.states[index, :] = state
         self.actions[index] = action
@@ -32,13 +37,16 @@ class Memory:
         # find the random indeces of experiences
         buffer_size = self.count()
         rnd_indeces = np.random.choice(np.arange(buffer_size), size=batch_size, replace=False)
+        # increment the number of times the experience was seen
+        self.seen[rnd_indeces] = self.seen[rnd_indeces] + 1
         # retrieve the data
         states = self.states[rnd_indeces, :]
         actions = self.actions[rnd_indeces]
         rewards = self.rewards[rnd_indeces]
         next_states = self.next_states[rnd_indeces, :]
         terminated = self.terminated[rnd_indeces]
-        return states, actions, rewards, next_states, terminated
+        seen = self.seen[rnd_indeces]
+        return states, actions, rewards, next_states, terminated, seen
 
     def count(self):
         return self.counter if self.counter < self.max_size else self.max_size
@@ -58,6 +66,7 @@ class Memory:
         for i in range(self.state_size):
             dataframe['next_state_{}'.format(i)] = self.next_states[:, i]
         dataframe['is_episode_finished'] = self.terminated
+        dataframe['seen'] = self.seen
         # save it
         dataframe.to_csv(file, index=False)
 
@@ -65,14 +74,28 @@ class Memory:
         '''Load memory from a csv file'''
         # the process of loading is done using the pandas dataframe
         dataframe = pd.read_csv(file)
-        for i in range(dataframe.shape[0]):
-            row = dataframe.iloc[i]
-            # add an experience to memory, faster way would probably be to insert the whole columns,
-            # but since this function is run in O(const.) times, it is not that important
-            self.add(
-                np.array(row[0:self.state_size]).reshape((1, -1)),
-                row[self.state_size],
-                row[self.state_size + 1],
-                np.array(row[self.state_size + 2:-1]).reshape((1, -1)),
-                row[-1]
-            )
+        if dataframe.shape[1] == 2 * self.state_size + 4:
+            for i in range(dataframe.shape[0]):
+                row = dataframe.iloc[i]
+                # add an experience to memory, faster way would probably be to insert the whole columns,
+                # but since this function is run in O(const.) times, it is not that important
+                self.add(
+                    np.array(row[0:self.state_size]).reshape((1, -1)),
+                    row[self.state_size],
+                    row[self.state_size + 1],
+                    np.array(row[self.state_size + 2:-2]).reshape((1, -1)),
+                    row[-2],
+                    row[-1]
+                )
+        else:
+            for i in range(dataframe.shape[0]):
+                row = dataframe.iloc[i]
+                # add an experience to memory, faster way would probably be to insert the whole columns,
+                # but since this function is run in O(const.) times, it is not that important
+                self.add(
+                    np.array(row[0:self.state_size]).reshape((1, -1)),
+                    row[self.state_size],
+                    row[self.state_size + 1],
+                    np.array(row[self.state_size + 2:-1]).reshape((1, -1)),
+                    row[-1]
+                )
