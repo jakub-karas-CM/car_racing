8d85fc8868c77808570d777a84a703662fc385b6
Jakub Karas <jakub.karas@mycroftmind.com>
2022-12-01 18:56:02 +0100

    comments added and logging used instead of print statements

--- q_learning.py
+++ q_learning.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 import numpy as np
 import pygame as pg
 import random
+import logging
 import path
 from memory import Memory
 from game import Game
-from car import CarActions
 
 class QLearning:
+    '''This class holds the functionality of the AI agent.'''
     def __init__(self, game: Game, memory_size, training_batch_size, pretrain_size, steps_to_align_target, learning_rate, gamma, min_epsilon = 0.1, max_epsilon = 1) -> None:
         # Initialize atributes
         self.game = game
@@ -48,9 +49,11 @@ class QLearning:
         self.target_network.set_weights(self.q_network.get_weights())
 
     def store(self, state, action, reward, next_state, terminated):
+        '''Store the experience to the memory.'''
         self.memory.add((state, action, reward, next_state, terminated))
 
     def pretrain(self):
+        '''Generate the needed random experiences to learn from.'''
         state = self.game.get_state()
         position = self.game.player.get_current_position()
         for _ in range(self.pretrain_size):
@@ -75,8 +78,9 @@ class QLearning:
                 position = next_position
 
     def retrain_q_network(self):
+        '''Back-propagation of the Q network. See any tutorial for explanation.'''
         # get training batch
-        states, actions, rewards, next_states, terminated = self.memory.sample(self.training_batch_size)
+        states, actions, rewards, next_states, terminated, seen = self.memory.sample(self.training_batch_size)
         # retrain network
         q_eval = self.q_network.predict(states, verbose=0)
         q_next = self.target_network.predict(next_states, verbose=0)
@@ -84,21 +88,24 @@ class QLearning:
         targets = q_eval.copy()
         batch_indices = np.arange(self.training_batch_size)
         targets[batch_indices, actions] = rewards + self.gamma * np.amax(q_next, axis=1) * (1 - terminated)
-        self.q_network.fit(states, targets, epochs=1, verbose=0)
+        self.q_network.fit(states, targets * (1 / seen.reshape((-1, 1))), epochs=1, verbose=0)
 
-    def train(self, training_episodes, max_steps, policy_steps, fps = 60):
+    def train(self, training_episodes, max_steps, policy_steps, save_after_every = 20, fps = 60):
+        '''The core of the Q learning algorithm.'''
         run = True
         clock = pg.time.Clock()
-        scores = []
-        epsilones = []
+        scores = [] # scores to compute training averages
         for episode in range(training_episodes):
+            # start of the episode
             self.game.reset()
             state = self.game.get_state()
             position = self.game.player.get_current_position()
             score = 0
             for _ in range(max_steps):
+                # go for at most `max_steps` steps
                 self.step += 1
                 for event in pg.event.get():
+                    # check for clicks on window's `X`
                     if event.type == pg.QUIT:
                         run = False
                         break
@@ -108,7 +115,7 @@ class QLearning:
                 reward = 0
                 for _ in range(policy_steps):                    
                     clock.tick(fps)
-                    # exercise the action n times
+                    # exercise the action `policy_steps` times
                     self.game.make_action(action)
                     next_position = self.game.player.get_current_position()
                     reward += self.get_reward(state[0][-1], position, next_position, state[0][-4])
@@ -131,40 +138,37 @@ class QLearning:
                     break
                 else:
                     if self.step >= self.steps_to_align_target:
+                        # align the second predicting network
                         self.step = 0
                         self.align_target_model()
-                        print("Target network aligned.")
+                        logging.getLogger().info("Target network aligned.")
                     state = next_state
                     position = next_position
             if not run:
-                break
-
-            epsilones.append(self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(-self.decay))
-            scores.append(score)
-            
-            if episode % 20 == 0:
+                # break if the window's `X` was clicked
+                break         
+            if episode % save_after_every == 0:
                 self.save(f"episode_{episode}", memory=True)
+            # log the episode score
+            scores.append(score)   
+            logging.getLogger().info(f"Episode {episode} is done. Score: {score}, avg score: {np.mean(scores[max(0, episode - 100):(episode+1)])}")
 
-            print(f"Episode {episode} is done. Score: {score}, avg score: {np.mean(scores[max(0, episode - 100):(episode+1)])}")
-
-    def run_episode():
-        pass
-
-    def get_action(self, state):            
-        self.memory.count()
-        self.decay += 1
+    def get_action(self, state):
+        '''Get an action, either the best one or random one.'''
+        self.decay += 1 # increase the epsilon decay to reduce it
         if np.random.rand() <= self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(-self.decay):
-            print("Random action!")
+            logging.getLogger().info("Random action!")
             return random.randint(0, self.action_size - 1)
         
-        q_values = self.q_network.predict(state, verbose=0)
+        q_values = self.q_network.predict(state, verbose=0) # find the best function
         return np.argmax(q_values[0])
 
     def get_reward(self, relative_distance_to_reward, current_position, next_position, relative_speed):
+        '''Get reward or punishment for a step. This part is horribly experimental, you may want to choose different parameters or the whole system.'''
         collistion_punishment = -100
         gate_reward = -collistion_punishment / 10
 
-        reward = -gate_reward * (relative_distance_to_reward) / 40 # + gate_reward * relative_speed
+        reward = -gate_reward * (relative_distance_to_reward ** 2) * (1 - relative_speed ** 2) / 40
         if current_position[0] == next_position[0] and current_position[1] == next_position[1]:
             reward += collistion_punishment / 10
         if self.game.gate_collision():
@@ -175,6 +179,7 @@ class QLearning:
         return reward
 
     def save(self, dir = None, memory = True):
+        '''Save the agent and memory to files.'''
         if not dir:
             dir = 'episode_{}'.format(self.game.game_number)
         # check if directory exists and remove all files if it does
@@ -187,13 +192,15 @@ class QLearning:
             self.memory.save(path.MEMORY / (dir + '.csv'))
 
     def load(self, dir, model_only = False):
+        '''Load an agent and its memory from files.'''
         # check directories
         if not (path.MODELS / dir).exists():
             raise FileExistsError("Directory for model doesn't exist.")
         if not (path.MEMORY / (dir + '.csv')).exists() and not model_only:
             raise FileNotFoundError("The memory storage file doesn't exist.")
-        
+        # load models
         self.q_network = tf.keras.models.load_model(path.MODELS / dir)
         self.target_network = tf.keras.models.load_model(path.MODELS / dir)
+        # load memory
         if not model_only:
             self.memory.load(path.MEMORY / (dir + '.csv'))
